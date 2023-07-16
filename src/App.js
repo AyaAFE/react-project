ab5344fe1255bf35100fb4f0c7ac3a3ad1f064e2
Aya Ayman <102162035+AyaAFE@users.noreply.github.com>
2023-07-16 16:21:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Firstcomponant from "./Firstcomponant";
+function App() {
+  return (
+    <div>
+      <h2 className="title">Hello React Js beginners from start come on</h2>
+
+      <Firstcomponant />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Firstcomponant.jsx
@@ -0,0 +1,8 @@
+const Firstcomponant = () =>{
+    return(
+        <div>
+            <h1> Hello React woho koookoooo </h1>
+        </div>
+    )
+}
+export default Firstcomponant;
