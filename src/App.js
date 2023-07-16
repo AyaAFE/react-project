27c9571904cab44ce5fccb60c8f7737230090b66
Aya Ayman <102162035+AyaAFE@users.noreply.github.com>
2023-07-16 16:34:13 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div>
       <h2 className="title">Hello React Js beginners from start come on</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis nobis omnis eveniet quia asperiores! Accusamus iste possimus error modi, cupiditate voluptates quisquam commodi ullam nesciunt, nisi recusandae accusantium quia eligendi.</p>
 
       <Firstcomponant />
     </div>
